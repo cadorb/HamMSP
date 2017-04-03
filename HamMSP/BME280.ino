e28277595aa65fcd8304d81b2309d2c71cd54cd9
Termiton <cadorb@cadorb.fr>
2017-04-03 23:25:53 +0200

    build project with platformio

---
+++ HamMSP/BME280.ino
@@ -0,0 +1,28 @@
+#include <Arduino.h>
+
+void getBME280(){
+float pressure;
+  
+  //get and print temperatures
+  Serial.print("Temp: ");
+  Serial.print(bme280.getTemperature());
+  Serial.println("C");//The unit for  Celsius because original arduino don't support speical symbols
+  Ciao.write(CONNECTOR, TOPIC_TEMP, String(bme280.getTemperature())); // pushes data into a channel
+  
+  //get and print atmospheric pressure data
+  Serial.print("Pressure: ");
+  Serial.print(pressure = bme280.getPressure());
+  Serial.println("Pa"); 
+  Ciao.write(CONNECTOR, TOPIC_PRES, String(bme280.getPressure()));
+
+  //get and print altitude data
+  Serial.print("Altitude: ");
+  Serial.print(bme280.calcAltitude(pressure));
+  Serial.println("m");
+
+  //get and print humidity data
+  Serial.print("Humidity: ");
+  Serial.print(bme280.getHumidity());
+  Serial.println("%");
+  Ciao.write(CONNECTOR, TOPIC_HUMI, String(bme280.getHumidity()));
+}
---
+++ HamMSP/HamMSP.ino
@@ -0,0 +1,49 @@
+#include <Arduino.h>
+
+#include <Wire.h>
+
+// Wifi - MQTT
+#include <UnoWiFiDevEd.h>
+
+// Temp pressure humidity
+#include "Seeed_BME280.h"
+
+// IMU
+#include "I2Cdev.h"
+#include "MPU9250.h"
+
+// Constants
+#include "Constants.c"
+
+// LED
+#include "ChainableLED.h"
+
+// Init temp pressure humidity
+BME280 bme280;
+
+// Init led
+ChainableLED leds(4, 5, NUM_LEDS);
+
+const int ledPin=12;
+
+void setup()
+{
+  Ciao.begin();
+  Serial.begin(9600);
+  if(!bme280.init()){
+    Serial.println("BME280 error!");
+  }
+  leds.init();
+  pinMode(ledPin,OUTPUT);
+}
+
+void loop()
+{
+  secure();
+  getBME280();
+  
+  
+  
+
+  delay(100);
+}
