7b1674e328e52cfb0641985675c6d707acc5f3c7
Blaise Cador <blaise.cador@gmail.com>
2017-05-18 21:59:41 +0200

    string problem

--- HamMSP/HamMSP.ino
+++ HamMSP/HamMSP.ino
@@ -20,16 +20,55 @@ BME280 bme280;
 
 // Init led
 ChainableLED leds(2,3, NUM_LEDS);
-
 const int ledPin=12;
-
 int windStatus;
+
+void meteoCheck(Task* me);
+void windSpeedCheck(Task* me);
+void windDirCheck(Task* me);
+
+
+// -- meteoCheck will be launched on every 2 seconds.
+Task taskMeteo(30000, meteoCheck);
+// -- windDirCheck will be launched on every 3 seconds.
+Task taskWindDir(10000, windDirCheck);
+// -- windSpeedCheck will be launched on every 5000 milliseconds.
+Task taskWindSpeed(WINDSPEED, windSpeedCheck);
+
+
+void setup()
+{
+  Serial.begin(9600);
+  delay(1000);
+  Serial.println("Starting Arduino");
+
+  Ciao.begin();
+  if(!bme280.init()){
+    Serial.println("BME280 error!");
+  }
+  Serial.println(String(bme280.getTemperature()));
+  leds.init();
+  leds.setColorRGB(0, 0, 15, 0);
+  pinMode(ledPin,OUTPUT);
+
+
+  // -- Register the tasks to the timer manager. Both tasks will start immediately.
+  SoftTimer.add(&taskMeteo);
+  SoftTimer.add(&taskWindDir);
+  SoftTimer.add(&taskWindSpeed);
+}
+
 
 
 void meteoCheck(Task* me){
   leds.setColorRGB(0, 25, 25, 0);
-  Serial.println("#Checking meteo ");
-  getBME280();
+  Serial.print("#Checking meteo ");
+  float temperature = bme280.getTemperature();
+  Serial.println(temperature);
+  Serial.println(String(temperature));
+  Ciao.write(CONNECTOR, TOPIC_TEMP, String(temperature)); // pushes data into a channel
+  Ciao.write(CONNECTOR, TOPIC_PRES, String(bme280.getPressure()));
+  Ciao.write(CONNECTOR, TOPIC_HUMI, String(bme280.getHumidity()));
 }
 
 void windDirCheck(Task* me){
@@ -53,33 +92,4 @@ void windSpeedCheck(Task* me){
     Serial.println("Le vent est faible");
     leds.setColorRGB(0, 0, 15, 0);
   }
-}
-
-// -- meteoCheck will be launched on every 2 seconds.
-Task taskMeteo(30000, meteoCheck);
-// -- windDirCheck will be launched on every 3 seconds.
-Task taskWindDir(10000, windDirCheck);
-// -- windSpeedCheck will be launched on every 5000 milliseconds.
-Task taskWindSpeed(WINDSPEED, windSpeedCheck);
-
-
-void setup()
-{
-  Serial.begin(9600);
-  delay(1000);
-  Serial.println("Starting Arduino");
-
-  Ciao.begin();
-  if(!bme280.init()){
-    Serial.println("BME280 error!");
-  }
-  leds.init();
-  leds.setColorRGB(0, 0, 15, 0);
-  pinMode(ledPin,OUTPUT);
-
-
-  // -- Register the tasks to the timer manager. Both tasks will start immediately.
-  SoftTimer.add(&taskMeteo);
-  SoftTimer.add(&taskWindDir);
-  SoftTimer.add(&taskWindSpeed);
 }
